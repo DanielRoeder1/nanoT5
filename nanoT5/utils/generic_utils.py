5570d8ead4e5bd1456c0bbe086f073b1ff44925c
julio-py <63744748+julio-py@users.noreply.github.com>
2023-05-27 13:01:00 +0200

    changed config loading to omegaconf

--- nanoT5/utils/generic_utils.py
+++ nanoT5/utils/generic_utils.py
@@ -1,28 +1,12 @@
-import yaml
-from types import SimpleNamespace
 import os
-
-# YAML -> SimpleNamespace
-# from: https://gist.github.com/jdthorpe/313cafc6bdaedfbc7d8c32fcef799fbf
-def load_config(config_path):
-    with open(config_path, 'r') as f:
-        config = yaml.load(f, Loader=Loader)
-    return config
-
-class Loader(yaml.Loader):
-    pass
-
-def _construct_mapping(loader, node):
-    loader.flatten_mapping(node)
-    return SimpleNamespace(**dict(loader.construct_pairs(node)))
-
-Loader.add_constructor(
-    yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG, _construct_mapping
-)
+from omegaconf import OmegaConf
 
 def get_args(config_path = "default"):
   if config_path == "default":
     dir_path = os.path.dirname(__file__)
-    config_path = config_path = os.path.join(dir_path, "../configs/default.yaml")
+    config_path = os.path.join(dir_path, "../configs/default.yaml")
   
-  return load_config(config_path)
+  return OmegaConf.load(config_path)
+
+
+
