035a04deed798152779cf1227eb307c860f8d776
julio-py <63744748+julio-py@users.noreply.github.com>
2023-06-01 16:14:32 +0200

    added wandb key loading

--- nanoT5/utils/generic_utils.py
+++ nanoT5/utils/generic_utils.py
@@ -2,11 +2,15 @@ import os
 from omegaconf import OmegaConf
 
 def get_args(config_path = "default"):
+  dir_path = os.path.dirname(__file__)
   if config_path == "default":
-    dir_path = os.path.dirname(__file__)
     config_path = os.path.join(dir_path, "../configs/default.yaml")
   
-  return OmegaConf.load(config_path)
+  args = OmegaConf.load(config_path)
 
-
-
+  if args.logging.wandb:
+    if args.logging.wandb_credential_path == 'default':
+        args.logging.wandb_credential_path = os.path.join(dir_path, "../configs/wandb_key.txt")
+    with open(args.logging.wandb_credential_path, "r") as f:
+      args.logging.wandb_key = f.read().strip()
+  return args 
