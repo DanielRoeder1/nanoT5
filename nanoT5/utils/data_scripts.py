2aef8c13b58c30d6d66d319a72d137bb02b2db7a
julio-py <63744748+julio-py@users.noreply.github.com>
2023-06-02 16:17:37 +0200

    rework sep_token, label_masking, tokenizer loading

--- nanoT5/utils/data_scripts.py
+++ nanoT5/utils/data_scripts.py
@@ -1,31 +1,33 @@
 import pandas as pd
 from tqdm import tqdm
 from datasets import Dataset
+from transformers import AutoTokenizer
 import os
 
 ######################################################
 # Functions for tokenizing MSMACRO and GPTDolly data #
 ######################################################
 
-def get_dataset(data_path, 
+def get_dataset(args, 
                 tokenizer,
-                args,
-                mode = "q_p_a", 
                 column_lookup = {"query": "instruction", "passage": "context", "response": "response"}, 
-                know_tokenizer = None, 
                 save_data = True):
-    if know_tokenizer is None: know_tokenizer = tokenizer
-    data = pd.read_csv(data_path).dropna()
+    if args.model.mode == "q_p_a" and args.model.know_enc_name != "T5":
+        assert args.model.know_enc_name is not None, "know_enc_name must be specified for mode q_p_a"
+        know_tokenizer = AutoTokenizer.from_pretrained(args.model.know_enc_name) 
+    else:
+        know_tokenizer = tokenizer
+    data = pd.read_csv(args.data.data_dir).dropna()
     process_dataset = []
     for query, passage, answer in tqdm(zip(data[column_lookup["query"]].values, data[column_lookup["passage"]].values, data[column_lookup["response"]].values)):
-            inputs = data_switching(mode, query, passage, answer, tokenizer)
+            inputs = data_switching(args.model.mode, query, passage, answer, tokenizer)
             if inputs is not None:
                 process_dataset.append(inputs)
     df = Dataset.from_list(process_dataset)
     df = df.map(tokenize_function, batched=True, fn_kwargs={"tokenizer": tokenizer, "know_tokenizer": know_tokenizer}, remove_columns= df.column_names)
     df = df.train_test_split(test_size = args.data.test_size, seed = args.seed)
     if save_data:
-        save_path = os.path.join(os.path.dirname(data_path), "hf_processed_data")
+        save_path = os.path.join(os.path.dirname(args.data.data_dir), "hf_processed_data")
         df.save_to_disk(save_path)
     return df
 
@@ -34,8 +36,9 @@ def data_switching(mode, query, p, a, tokenizer):
     Changes data format based on mode input by user
     """
     assert mode in ["q_a", "qp_a", "q_pa","q_p_a"], "select one of the following modes: q_a, qp_a, q_pa, q_p_a"
-    if mode in ["q_pa", "qp_a"]: assert tokenizer.sep_token_id is not None, "tokenizer must have a sep_token_id for mode q_pa or qp_a"
-    sep_token = tokenizer.sep_token if tokenizer.sep_token_id is not None else None
+    if mode in ["q_pa", "qp_a"]: 
+        assert tokenizer.sep_token_id is not None, "tokenizer must have a sep_token_id for mode q_pa or qp_a"
+        sep_token = tokenizer.sep_token
     query,p,a = query.strip(), p.strip(), a.strip()
     if mode == "q_a":
         output =  {'input_ids': query, 'labels': a}
@@ -44,10 +47,8 @@ def data_switching(mode, query, p, a, tokenizer):
         output =  {'input_ids': query_p, 'labels': a}
     elif mode == "q_pa":
         a_p = p + " " + sep_token + " " + a
-        output =  {'input_ids': query, 'decoder_input_ids': a_p}
+        output =  {'input_ids': query, 'labels': a_p}
     elif mode == "q_p_a":
-        if sep_token is not None:
-            p = sep_token + " " + p
         output =  {'input_ids': query, 'p_input_ids':p, 'labels': a}
     # Remove samples that are longer than the model max length
     if any([True if len(tokenizer.tokenize(v)) > tokenizer.model_max_length  else False for v in output.values()]): 
@@ -60,7 +61,8 @@ def tokenize_function(inputs, tokenizer, know_tokenizer):
     """
     t_lookup = {"input_ids": tokenizer, "labels": tokenizer, "decoder_input_ids": tokenizer, "p_input_ids": know_tokenizer}
     tokenized_input = {k: t_lookup[k](v, truncation = True).input_ids for k,v in inputs.items()}
-    if "decoder_input_ids" in tokenized_input:
-        labels  = tokenized_input["decoder_input_ids"].copy()
-        tokenized_input['labels'] = [[-100 if i < l_i else  l for i,l in enumerate(label)] for label in labels if (l_i:=label.index(tokenizer.sep_token_id)) >0]
+    
+    # This would mask the context in the labels resulting on no loss being calculated for the context
+    #if mask_context:
+    #    tokenized_input['labels'] = [[-100 if i < l_i else  l for i,l in enumerate(label)] for label in tokenized_input['labels'] if (l_i:=label.index(tokenizer.sep_token_id)) >0]
     return tokenized_input
