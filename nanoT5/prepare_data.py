f19c9f55f71bb6c96737c053d1ba214d2ca5589c
julio-py <63744748+julio-py@users.noreply.github.com>
2023-06-05 11:44:57 +0200

    prepare_data update to new get_data func

--- nanoT5/prepare_data.py
+++ nanoT5/prepare_data.py
@@ -8,12 +8,10 @@ from transformers import AutoTokenizer
 def prepare_data():
   args = get_args()
   tokenizer = get_tokenizer(args)
-  if args.model.knowledge_injection:
-    know_tokenizer = AutoTokenizer.from_pretrained(args.model.know_enc_name) if args.model.know_enc_name != "T5" else tokenizer
-  dataset = get_dataset(args.data.data_dir, tokenizer, args, know_tokenizer=know_tokenizer, save_data=False)
+  dataset = get_dataset(args, tokenizer, save_data=True)
   print(dataset)
   return dataset
 
 if __name__ == "__main__":
   dataset = prepare_data()
-  dataset.save_to_disk("/content/sample_data/hf_data")
+  #dataset.save_to_disk("/content/sample_data/hf_data")
