d82d013fd7560b1346a266723230e65011b982a3
julio-py <63744748+julio-py@users.noreply.github.com>
2023-06-06 22:50:41 +0200

    fix for multi mode

--- nanoT5/utils/generic_utils.py
+++ nanoT5/utils/generic_utils.py
@@ -44,8 +44,7 @@ def get_args(config_path = None, load_cmd =  True):
      args.model.mode = cmd_args.mode
 
   args.best_eval_loss = float('inf')
-
-  if not isinstance(args.model.mode, list):
+  if isinstance(args.model.mode, str):
     args.model.mode = [args.model.mode]
   return args 
 
